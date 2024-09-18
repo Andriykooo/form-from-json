0da202330117d28c0b52bcf7106130f1ca87623c
Andriykooo <boichukandri9@gmail.com>
2024-09-18 22:54:38 +0300

    changed background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Form } from "@/components/Form/Form";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 pb-20 sm:p-20 bg-slate-950 font-[family-name:var(--font-geist-sans)] text-white">
+    <div className="min-h-screen p-8 pb-20 sm:p-20 bg-black font-[family-name:var(--font-geist-sans)] text-white">
       <div className="flex items-center mb-4 gap-4">
         <h1 className="font-bold text-xl">Form</h1>
       </div>
